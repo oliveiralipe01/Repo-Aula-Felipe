a654fce5896656ea1a935e62c07a9b87e3f3dac4
oliveiralipe01 <felipecoder01@gmail.com>
2026-03-20 20:34:26 -0300

    add - funcoes em C

---
+++ funcoes.c
@@ -0,0 +1,15 @@
+int soma(int n, int n2){
+    return n = n2;
+} 
+
+int divisão(int n1, int n2){
+    return n1/n2;
+}
+
+int main(){
+    int n, result;
+    result = soma(n,10);
+    printf("%d", result);
+    result = divisão(n,10);
+    printf("%d", result);
+}
